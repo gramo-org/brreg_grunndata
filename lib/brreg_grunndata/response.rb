42271326310ddef618d8ecde528414a7bdfa061a
Thorbjørn Hermansen <thhermansen@gmail.com>
2016-11-29 14:49:23 +0100

    Class for wrapping brreg's response_header.
    It extracts status and sub statuses.

--- lib/brreg_grunndata/response.rb
+++ lib/brreg_grunndata/response.rb
@@ -1,6 +1,7 @@
 require 'forwardable'
 
 require_relative 'utils'
+require_relative 'response_header'
 
 module BrregGrunndata
   # Wrapper around Savon's response
@@ -11,23 +12,75 @@ module BrregGrunndata
 
     extend Forwardable
     def_delegators :@savon_response,
-                   :success?, :soap_fault?, :http_error?,
-                   :body, :to_hash
+                   :soap_fault?, :http_error?,
+                   :body
 
-    class UnwrapError < Error; end
+    # Error raised whenever soap or http error occured so
+    # the success? is false but you are still asking for
+    # either header or message
+    class ResponseFailureError < Error; end
+
+    # Error raised when soap communication was a success,
+    # brreg response header indicates success, but for some
+    # reason we didn't get any return value as we expected
+    #
+    # Inspecting #header before calling #message will determin if this error
+    # is to be expected.
+    #
+    # @seehttps://www.brreg.no/produkter-og-tjenester/bestille/tilgang-til-enhetsregisteret-via-web-services/teknisk-beskrivelse-web-services/grunndataws/
+    class MessageEmptyError < Error; end
 
     def initialize(savon_response)
       @savon_response = savon_response
     end
 
-    def unwrap
-      raise UnwrapError unless success?
+    # Do we have a successful response?
+    #
+    # We do if:
+    #   - We have no HTTP errors or SOAP faults. Savon handles this for us.
+    #   - We have a response_header in the document returned from brreg
+    #     which indicates success too. I don't know why BRREG needs this in
+    #     addition to HTTP status codes and SOAP faults to communicate success
+    #     or not :-(
+    def success?
+      @savon_response.success? && header.success?
+    end
 
-      return_xml = body.values.first[:return]
-      parse(return_xml)
+    # Returns the header from brreg's response
+    #
+    # The header contains the overall success of the SOAP operation.
+    # The header is something brreg's SOAP API has, so all in all we have
+    # http status, soap status and then this status located within the header :(
+    def header
+      raise ResponseFailureError unless @savon_response.success?
+
+      ResponseHeader.new response_grunndata[:response_header]
+    end
+
+    # Returns the header from brreg's response
+    #
+    # Keys are symbolized.
+    #
+    # @return Hash
+    def message
+      raise ResponseFailureError unless success?
+
+      @message ||= begin
+        message = response_grunndata[:melding]
+
+        if message.nil?
+          raise MessageEmptyError
+        end
+
+        message
+      end
     end
 
     private
+
+    def response_grunndata
+      @response_grunndata ||= parse(body.values.first[:return])[:grunndata]
+    end
 
     def parse(xml)
       parser = Nori.new(
---
+++ lib/brreg_grunndata/response_header.rb
@@ -0,0 +1,44 @@
+module BrregGrunndata
+  # Represents a response header from brreg
+  #
+  # A response header has a main status and sub statuses.
+  #
+  # MAIN STATUS:
+  #   0   -   OK
+  #   1   -   OK, but some data is missing. See sub status for details
+  #  -1   -   An error as occured
+  #
+  # @see https://www.brreg.no/produkter-og-tjenester/bestille/tilgang-til-enhetsregisteret-via-web-services/teknisk-beskrivelse-web-services/grunndataws/
+  class ResponseHeader
+    MAIN_STATUS_SUCCESS_CODES = [0, 1].freeze
+
+    def initialize(nori_response_header)
+      @nori_response_header = nori_response_header
+    end
+
+    # Returns true if the brreg response header indicates success.
+    def success?
+      MAIN_STATUS_SUCCESS_CODES.include? main_status
+    end
+
+    def main_status
+      @main_status ||= int(@nori_response_header[:hoved_status])
+    end
+
+    def sub_statuses
+      @sub_statuses ||= @nori_response_header[:under_status][:under_status_melding].map do |status|
+        {
+          code: int(status.attributes['kode']),
+          message: status.to_s
+        }
+      end
+    end
+
+    private
+
+    # Cast value to int
+    def int(v)
+      Integer v, 10
+    end
+  end
+end
