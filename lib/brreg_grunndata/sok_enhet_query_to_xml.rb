f66a35eab6854ab4adf623f4bc78687b2a2282b7
Thorbjørn Hermansen <thhermansen@gmail.com>
2016-12-02 15:26:41 +0100

    The search actually works.
    Seems like the XML declaration was not working well. Without it we get a
    return value. According to the docs the expected encoding is UTF-8 in
    the search request if you don't declare it.

--- lib/brreg_grunndata/sok_enhet_query_to_xml.rb
+++ lib/brreg_grunndata/sok_enhet_query_to_xml.rb
@@ -1,3 +1,5 @@
+require 'builder'
+
 module BrregGrunndata
   # Translate a simple query string to an XML document to search with sok_enhet operation
   #
@@ -22,27 +24,36 @@ module BrregGrunndata
   # payload instead of something simple ;-)
   #
   class SokEnhetQueryToXml
-    def initialize(query, first: 100)
+    def initialize(query, first: 100, ip: '010.001.052.011')
       @query = query
       @first = first
+      @ip = ip
     end
 
     def cdata
-      <<~XML
-        <![CDATA[
-          <?xml version="1.0"?>
-          <BrAixXmlRequest RequestName="BrErfrSok">
-            <BrErfrSok>
-              <BrSokeStreng>#{@query}</BrSokeStreng>
-              <MaxTreffReturneres>#{@first}</MaxTreffReturneres>
-              <ReturnerIngenHvisMax>true</ReturnerIngenHvisMax>
-              <RequestingIPAddr>010.001.052.011</RequestingIPAddr>
-              <RequestingTjeneste>SOAP</RequestingTjeneste>
-              <MedUnderenheter>true</MedUnderenheter>
-            </BrErfrSok>
-          </BrAixXmlRequest>
-        ]]>
-      XML
+      "<![CDATA[#{xml}]]>"
+    end
+
+    private
+
+    def xml
+      data = {
+        br_aix_xml_request: {
+          :@RequestName => 'BrErfrSok',
+          br_erfr_sok: {
+            br_soke_streng: @query,
+            max_treff_returneres: @first,
+            returner_ingen_hvis_max: true,
+            requesting_IP_addr: @ip,
+            requesting_tjeneste: 'SOAP',
+            med_underenheter: true
+          }
+        }
+      }
+
+      options = { key_converter: :camelcase }
+
+      Gyoku.xml data, options
     end
   end
 end
